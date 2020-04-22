1f39d536eee895518c4d4a4e4863407d9db441ab
LuizLeandrini <guilhermeleandrini@hotmail.com>
2020-04-22 14:37:37 -0300

    First Commit

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Luiz Guilherme on 22/04/20.
+//  Copyright © 2020 Luiz Guilherme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
+
