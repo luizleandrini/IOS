9e78b4e805a8d590ee207a0d7a13c71f5fda61a3
LuizLeandrini <guilhermeleandrini@hotmail.com>
2020-05-13 13:34:41 -0300

    Quiz final

--- SwiftQuiz/SwiftQuiz/Quiz.swift
+++ SwiftQuiz/SwiftQuiz/Quiz.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class Quiz {
-    <#code#>
+    let question: String
+    let options: [String]
+    private let correctedAnswer: String
+    
+    init(question: String, options: [String], correctedAnswer: String) {
+        self.question = question
+        self.options = options
+        self.correctedAnswer = correctedAnswer
+    }
+    
+    func validateOption(_ index: Int) -> Bool{
+        let answer = options[index]
+        return answer == correctedAnswer
+    }
+    deinit {
+        print("Liberou quiz da memoria")
+    }
 }
--- SwiftQuiz/SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/SwiftQuiz/ResultViewController.swift
@@ -15,15 +15,23 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var lbWrong: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     
-    
+    var totalCorrectAnswers: Int = 0
+    var totalAnswers: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        lbAnswer.text = "Perguntas respondidas: \(totalAnswers)"
+        lbCorrect.text = "Perguntas cprretas: \(totalCorrectAnswers)"
+        lbWrong.text = "Perguntas erradas: \(totalAnswers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers*100/totalAnswers
+        labelScore.text = "\(score)%"
+        
     }
     
     @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
